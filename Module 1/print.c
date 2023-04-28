41d3be45e6cc6191095c7e725416d9b1f3a78b44
Md Forkan H F <118150406+MdForkanHF@users.noreply.github.com>
2023-04-28 11:31:08 +0530

    Add files via upload

---
+++ Module 1/print.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+    printf("hellow world \n");
+    printf("i'm a programmer \n");
+    printf("notun line e jao\n\n");
+
+    printf("hellow world\ni'm a programmer\nnotun line e jao");
+
+}
+
+
+
+//  \n is useed for new line
+// ctrl + / = eta diye selected line comment hoye jai
+// && and  \\ = e 2 ta specia; charecter, eula ekta show korar jonno 2 ta lekha lage
---
+++ Module 1/variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int rahim, karim;
+    char c;
+    float f;
+    scanf("%d %f %c", &rahim, &f, &c);
+    printf("%d %.2f %c", rahim, f, c);
+    return 0;
+}
+
+
